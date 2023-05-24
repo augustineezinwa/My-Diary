91e523b7a5be4faa44447b479cf2d96e708a35c8
Augustine Ezinwa <augustineezinwa@gmail.com>
2023-05-24 05:28:59 +0100

    make s3 upload work

--- API/Controllers/PhotoUploadController.js
+++ API/Controllers/PhotoUploadController.js
@@ -1,17 +1,71 @@
-import cloudinary from 'cloudinary';
-import dotenv from 'dotenv';
+import {
+  PutObjectCommand,
+  S3,
+} from "@aws-sdk/client-s3";
+
+import dotenv from "dotenv";
 
 dotenv.config();
+
+const REGION = "us-east-1";
+const s3Client = new S3({
+  region: REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 
 class PhotoUploadController {
   static async uploadPhoto(request, response) {
     try {
-      const result = await cloudinary.v2.uploader.upload(request.body.picture);
+      const bucketName = process.env.AWS_BUCKET_NAME;
+      const fileName = `${Date.now().toString()}_${request.body.fileName}`;
 
-      return response.json({ url: result.secure_url });
+      const fileUniqueKey = fileName.replace(/\s/g, '_').replace(/\.[^.]*$/, '');
+      const matches = request.body.picture.match(/^data:(.+);base64,(.+)$/);
+      const type = matches[1];
+
+      await s3Client.send(
+        new PutObjectCommand({
+          Bucket: bucketName,
+          Key: fileUniqueKey,
+          Body: Buffer.from(matches[2], 'base64'),
+          ContentEncoding: 'base64',
+          ContentType: type,
+        })
+      );
+
+      console.log(`https://${bucketName}.s3.${REGION}.amazonaws.com/${fileUniqueKey}`);
+      return response.json({ url: `/api/v1/image/${fileUniqueKey}` });
     } catch (e) {
+      console.log(e);
       return response.status(500).json({
+        error: e.toString(),
+      });
+    }
+  }
 
+  static async getPhoto(request, response) {
+    try {
+      const bucketName = process.env.AWS_BUCKET_NAME;
+      const { id } = request.params;
+
+      // Set the desired request headers
+
+      const res = await s3Client.getObject({
+        Bucket: bucketName,
+        Key: id,
+      });
+      const bodyStream = res.Body;
+
+      response.setHeader('Content-Type', res.ContentType);
+      response.setHeader('Cache-Control', 'public, max-age=31536000'); // Cache for 1 year (adjust the max-age value as needed)
+      response.setHeader('Expires', new Date(Date.now() + 31536000000).toUTCString()); // Expires in 1 year
+
+      return bodyStream.pipe(response);
+    } catch (e) {
+      // console.log(e, 'error');
+      // console.log(request.body);
+      return response.status(500).json({
         error: e.toString(),
       });
     }
--- API/Routes/PhotoUploadRouter.js
+++ API/Routes/PhotoUploadRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import PhotoUploadController from '../Controllers/PhotoUploadController';
+import PhotoUploadController from '../Controllers/PhotoUploadController.js';
 
 // instantiate router from express
 const PhotoUploadRouter = express.Router();
 
 PhotoUploadRouter.post('/api/v1/image/upload', PhotoUploadController.uploadPhoto);
+PhotoUploadRouter.get('/api/v1/image/:id', PhotoUploadController.getPhoto);
 
 export default PhotoUploadRouter;
